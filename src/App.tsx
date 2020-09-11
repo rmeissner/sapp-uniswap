d22133c8f28d4db6c8edfe24a0a41d0ef0c31ec3
Richard Meissner <richard@gnosis.io>
2020-09-11 18:07:42 +0200

    Add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import './App.css';
 import { useSafe } from '@rmeissner/safe-apps-react-sdk';
 import { Button } from '@gnosis.pm/safe-react-components';
+import { CircularProgress } from '@material-ui/core';
 
 const App = () => {
   const safe = useSafe()  
+  const [submitting, setSubmitting] = useState(false)
   const submitTx = useCallback(async () => {
+    setSubmitting(true)
     await safe.asyncSendTransactions([
       {
         "to": safe.getSafeInfo().safeAddress,
@@ -13,11 +16,18 @@ const App = () => {
         "data": "0x"
       }
     ])
-    console.log("Submitted")
+    setSubmitting(false)
   }, [safe])
   return <>
     {safe.getSafeInfo().safeAddress}<br/>
+    {submitting ? 
+    <>
+    <CircularProgress color="secondary" /> 
+    <Button size="lg" color="secondary" onClick={() => {setSubmitting(false)}}>Cancel</Button>
+    </>
+    : 
     <Button size="lg" color="primary" onClick={submitTx}>Submit</Button>
+    }
   </>
 }
 
